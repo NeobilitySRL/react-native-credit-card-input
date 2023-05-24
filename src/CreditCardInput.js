f281a4a34845d15617f3be40710cda62b3ede491
Andrei Radulescu <andi.radulescu@gmail.com>
2023-05-24 15:36:49 +0300

    Replaced deprecated componentWillReceiveProps with componentDidUpdate

--- src/CreditCardInput.js
+++ src/CreditCardInput.js
@@ -96,8 +96,8 @@ export default class CreditCardInput extends Component {
 
   componentDidMount = () => this._focus(this.props.focused);
 
-  componentWillReceiveProps = newProps => {
-    if (this.props.focused !== newProps.focused) this._focus(newProps.focused);
+  componentDidUpdate = prevProps => {
+    if (prevProps.focused !== this.props.focused) this._focus(this.props.focused);
   };
 
   _focus = field => {
