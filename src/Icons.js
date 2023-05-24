1daa2dd0aa865a005e64ea072c8406781af67b15
Andrei Radulescu <andi.radulescu@gmail.com>
2023-05-24 16:26:30 +0300

    Sorted Icons.js in a more logical order

--- src/Icons.js
+++ src/Icons.js
@@ -1,13 +1,13 @@
 const Icons = {
-  cvc: require("./icons/stp_card_cvc.png"),
-  cvc_amex: require("./icons/stp_card_cvc_amex.png"),
+  visa: require("./icons/stp_card_visa.png"),
+  mastercard: require("./icons/stp_card_mastercard.png"),
   "american-express": require("./icons/stp_card_amex.png"),
   "diners-club": require("./icons/stp_card_diners.png"),
-  mastercard: require("./icons/stp_card_mastercard.png"),
   discover: require("./icons/stp_card_discover.png"),
   jcb: require("./icons/stp_card_jcb.png"),
+  cvc: require("./icons/stp_card_cvc.png"),
+  cvc_amex: require("./icons/stp_card_cvc_amex.png"),
   placeholder: require("./icons/stp_card_unknown.png"),
-  visa: require("./icons/stp_card_visa.png"),
 };
 
 export default Icons;
